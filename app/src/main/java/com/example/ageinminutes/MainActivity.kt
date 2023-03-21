af0934da2d8060305b7f954e47c9a47f8568f0ff
TahmidTaki <tahmid.taki@live.iium.edu.my>
2023-03-21 16:19:38 +0800

    finalize project

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -6,8 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -29,9 +31,41 @@ class MainActivity : AppCompatActivity() {
         val i2=myCalendar.get(Calendar.MONTH)
         val i3=myCalendar.get(Calendar.DAY_OF_MONTH)
         
-        DatePickerDialog(this,
+        val dpd=DatePickerDialog(this,
             DatePickerDialog.OnDateSetListener
-        { view, i, i2, i3 ->  },i,i2,i3).show()
+        { view, i, i2, i3 ->
+
+            Toast.makeText(this,
+                "Year: $i, Month: ${i2+1}, Day: $i3",
+                Toast.LENGTH_LONG).show()
+
+            val selectedDate="$i3/${i2 + 1}/$i"
+            val sdf=SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+            val theDate=sdf.parse(selectedDate)
+
+            val tvSelectedDate=findViewById<TextView>(R.id.selectedDate)
+            tvSelectedDate.setText("$theDate")
+
+
+            val selectedDateInMinutes=theDate!!.time / 60000
+
+            val currentDate=sdf.parse(sdf.format(System.currentTimeMillis()))!!.time/60000
+
+            val differenceInMinutes=currentDate-selectedDateInMinutes
+             val timePassedValue=findViewById<TextView>(R.id.timePassed);
+
+            timePassedValue.setText("$differenceInMinutes")
+
+
+
+
+        },i, i2, i3)
+
+        dpd.datePicker.setMaxDate(Date().time - 86400000)
+        dpd.show()
+
+
+
         
           }
 }
